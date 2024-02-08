3d8a42d26f8eae6258550466f855111c3d504c4e
MatheusY <matheus.matsubara12@gmail.com>
2024-02-08 07:04:03 +0000

    Authentication & Route protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,15 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const appRoute: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {
     path: 'recipes',
     component: RecipesComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: RecipeStartComponent },
       { path: 'new', component: RecipeEditComponent },
@@ -31,6 +34,7 @@ const appRoute: Routes = [
     path: 'shopping-list',
     component: ShoppingListComponent,
   },
+  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
